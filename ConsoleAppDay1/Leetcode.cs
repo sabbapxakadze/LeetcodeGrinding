507bec3dab203b31dea75ab2f45ce9618de4b72f
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-09-08 01:06:23 +0400

    CountBits + PowerOfFour

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -996,6 +996,42 @@ public class Leetcode
         return rem == 0 && IsPowerOfThree(n / 3);
     }
 
+    public int[] CountBits(int n)
+    {
+        if (n == 0)
+            return new int[] { 0 };
+
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            int j = i;
+            while (j != 0)
+            {
+                arr[i] += j & 1;
+                j >>= 1;
+            }
+        }
+        return arr;
+    }
+    public bool IsPowerOfFour(int n)
+    {
+        if (n < 1)
+            return false;
+        if (n == 1)
+            return true;
+        while (n != 0)
+        {
+            if (n % 4 != 0)
+                return false;
+            n /= 4;
+        }
+        return true;
+    }
+
+
+
+
     public static void Main(string[] args)
     {
         Leetcode l = new Leetcode();
