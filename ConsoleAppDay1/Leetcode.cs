fae331012f18d7aeb8eb6f86f27ef4ce325ca8cc
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-09-15 20:28:27 +0400

    RangeAdditionII + MinimumIndexSumOfTwoList + MergeTwoBinaryTrees + MaximumProductOfThree

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -1896,7 +1896,7 @@ public class Leetcode
         Array.Sort(nums);
 
         int res = 0;
-        for (int i = 0; i < nums.Length; i+= 2)
+        for (int i = 0; i < nums.Length; i += 2)
         {
             res += nums[i];
         }
@@ -1914,7 +1914,7 @@ public class Leetcode
                 return 0;
             if (memo.ContainsKey(root))
                 return memo[root];
-            
+
             memo[root] = root.val + TreeSum(root.left, memo) + TreeSum(root.right, memo);
             return memo[root];
         }
@@ -2044,8 +2044,81 @@ public class Leetcode
         }
         return max;
     }
+    public int MaxCount(int m, int n, int[][] ops)
+    {
+        if (ops == null || ops.Length == 0)
+            return 0;
+        int minM = m, minN = n;
+
+        foreach (var item in ops)
+        {
+            minM = Math.Min(item[0], minM);
+            minN = Math.Min(item[1], minN);
+        }
+        return minM * minN;
+    }
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+        if (list1 == null || list2 == null)
+            return Array.Empty<string>();
+
+        Dictionary<string, int> d = new Dictionary<string, int>();
+        for (int i = 0; i < list1.Length; i++)
+        {
+            d[list1[i]] = i;
+        }
+        Dictionary<string, int> m = new Dictionary<string, int>();
+        int minSum = int.MaxValue;
+        for (int i = 0; i < list2.Length; i++)
+        {
+            if (d.ContainsKey(list2[i]))
+            {
+                if (minSum > d[list2[i]] + i)
+                {
+                    minSum = d[list2[i]] + i;
+                    m[list2[i]] = minSum;
+                }
+            }
+        }
+        return m.Where(x => x.Value == minSum).Select(x => x.Key).ToArray();
+    }
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        if (root1 == null && root2 == null)
+            return null;
+        if (root1 == null)
+            return root2;
+        if (root2 == null)
+            return root1;
+
+        TreeNode left = MergeTrees(root1.left, root2.left);
+        TreeNode right = MergeTrees(root1.right, root2.right);
+        TreeNode newTree = new TreeNode(root1.val + root2.val, left, right);
+
+        return newTree;
+    }
+    public int MaximumProduct(int[] nums)
+    {
+        int max1 = int.MinValue, max2 = int.MinValue, max3 = int.MinValue;
+        int min1 = int.MaxValue, min2 = int.MaxValue;
+
+        foreach (int n in nums)
+        {
+            if (n > max1) { max3 = max2; max2 = max1; max1 = n; }
+            else if (n > max2) { max3 = max2; max2 = n; }
+            else if (n > max3) { max3 = n; }
+
+            if (n < min1) { min2 = min1; min1 = n; }
+            else if (n < min2) { min2 = n; }
+        }
+
+        return Math.Max(max1 * max2 * max3, max1 * min1 * min2);
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
+
+        Console.WriteLine(MaxCount(3, 3, [[2, 2], [3, 3], [3, 3], [3, 3], [2, 2], [3, 3], [3, 3], [3, 3], [2, 2], [3, 3], [3, 3], [3, 3]]));
+        Console.WriteLine();
     }
 }
