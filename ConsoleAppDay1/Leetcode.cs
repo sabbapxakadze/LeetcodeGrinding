fcf07da2646cbf1dccdc4445a4a9322ddc6c64e7
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-10-01 20:30:16 +0400

    ReorderList

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -4526,6 +4526,41 @@ public class Leetcode
         }
         return i;
     }
+    public void ReorderList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow.next;
+        while (curr != null)
+        {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        slow.next = null;
+
+        ListNode first = head, second = prev;
+        while (second != null)
+        {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+
+            first.next = second;
+            second.next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
