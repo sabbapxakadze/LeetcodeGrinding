833bc9c5921276de6c2aa7b8094efdfc40feb380
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-10-06 18:52:13 +0400

    NthTribonacciNumber

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -5292,6 +5292,22 @@ public class Leetcode
         }
         return lists;
     }
+    public int Tribonacci(int n)
+    {
+        if (n <= 0)
+            return 0;
+        if (n <= 2)
+            return 1;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        for (int i = 3; i < arr.Length; i++)
+        {
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+        }
+        return arr[n];
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
