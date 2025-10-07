f60d41cfb3181331fb72e5892c548a067019286d
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-10-07 21:48:04 +0400

    LongestCommonSubsequence

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -5457,6 +5457,23 @@ public class Leetcode
         int?[,] memo = new int?[grid.Length, grid[0].Length];
         return MinPath(0, 0, memo);
     }
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        if (string.IsNullOrEmpty(text1) || string.IsNullOrEmpty(text2))
+            return 0;
+        int[,] arr = new int[text1.Length + 1, text2.Length + 1];
+        for (int i = text1.Length - 1; i >= 0; i--)
+        {
+            for (int j = text2.Length - 1; j >= 0; j--)
+            {
+                if (text1[i] == text2[j])
+                    arr[i, j] = 1 + arr[i + 1, j + 1];
+                else
+                    arr[i, j] = Math.Max(arr[i + 1, j], arr[i, j + 1]);
+            }
+        }
+        return arr[0, 0];
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
