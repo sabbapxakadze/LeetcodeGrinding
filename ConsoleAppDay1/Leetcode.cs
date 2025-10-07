b772176d969826eb9a32a2e6a22c87bbd8674640
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-10-07 22:05:45 +0400

    IsValidSudoku

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -5474,6 +5474,32 @@ public class Leetcode
         }
         return arr[0, 0];
     }
+    public bool IsValidSudoku(char[][] board)
+    {
+        if (board == null || board.Length != 9) return false;
+
+        HashSet<char>[] rows = new HashSet<char>[9];
+        HashSet<char>[] cols = new HashSet<char>[9];
+        HashSet<char>[] boxes = new HashSet<char>[9];
+        for (int i = 0; i < 9; i++)
+        {
+            rows[i] = new HashSet<char>();
+            cols[i] = new HashSet<char>();
+            boxes[i] = new HashSet<char>();
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                char c = board[i][j];
+                if (c == '.') continue;
+                int boxIndex = (i / 3) * 3 + j / 3;
+                if (!rows[i].Add(c) || !cols[j].Add(c) || !boxes[boxIndex].Add(c))
+                    return false;
+            }
+        }
+        return true;
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
