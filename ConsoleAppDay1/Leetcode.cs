6911d15ebada1f4bad95d6644615b300479dd000
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-10-10 23:05:07 +0400

    SpiralMatrix

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -5838,6 +5838,43 @@ public class Leetcode
             Array.Reverse(matrix[i]);
         }
     }
+    public List<int> SpiralOrder(int[][] matrix)
+    {
+        if (matrix == null)
+            return new List<int>();
+
+        int left = 0, right = matrix[0].Length;
+        int top = 0, bottom = matrix.Length;
+        List<int> res = new List<int>();
+        while (left < right && top < bottom)
+        {
+            for (int i = left; i < right; i++)
+            {
+                res.Add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i < bottom; i++)
+            {
+                res.Add(matrix[i][right - 1]);
+            }
+            right--;
+            if (!(left < right && top < bottom))
+            {
+                break;
+            }
+            for (int i = right - 1; i >= left; i--)
+            {
+                res.Add(matrix[bottom - 1][i]);
+            }
+            bottom--;
+            for (int i = bottom - 1; i >= top; i--)
+            {
+                res.Add(matrix[i][left]);
+            }
+            left++;
+        }
+        return res;
+    }
     static void Main(string[] args)
     {
         Dictionary<int, int> d = new Dictionary<int, int>();
