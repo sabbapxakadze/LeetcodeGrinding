b9d609546dbf99b2d4c9ff2c8e439d33f1909d0a
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-11-17 16:12:47 +0400

    SingleElementInASortedArray

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -8421,6 +8421,29 @@ public class Leetcode
         Dfs(0, new HashSet<string>());
         return max;
     }
+    public int SingleNonDuplicate(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            return 0;
+        int l = 0, r = nums.Length - 1;
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+            if ((m & 1) == 1)
+            {
+                m--;
+            }
+            if (nums[m] != nums[m + 1])
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 2;
+            }
+        }
+        return nums[l];
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
