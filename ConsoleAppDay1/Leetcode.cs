f50537f0dadcc20de41a2019c0bc5fbd25ffdac0
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-11-19 00:01:22 +0400

    BinarySearchRevision

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -8541,6 +8541,33 @@ public class Leetcode
         Backtrack(new List<int>());
         return lists;
     }
+    public int SearchRevision(int[] nums, int target)
+    {
+        if (nums == null || nums.Length == 0)
+            return -1;
+        int l = 0, r = nums.Length - 1;
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[l] <= nums[mid])
+            {
+                if (nums[l] <= target && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else
+            {
+                if (target <= nums[r] && target > nums[mid])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
