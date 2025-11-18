1b83dee4b32c042e8f6c423dc0869fb67f662e05
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-11-18 18:24:56 +0400

    SearchInARotatedArrayII

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -8444,6 +8444,36 @@ public class Leetcode
         }
         return nums[l];
     }
+    public bool Search(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] == nums[l] && nums[r] == nums[mid])
+            {
+                l++;
+                r--;
+            }
+            else if (nums[l] <= nums[mid])
+            {
+                if (target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else
+            {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return false;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
