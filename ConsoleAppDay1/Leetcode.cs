bdc0c54dfe5463e68375873147f1ca5e9c98d7f3
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-11-19 20:12:10 +0400

    MinOpsToMakeAnArrayIncreasing

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -8589,7 +8589,21 @@ public class Leetcode
             count += prefixSum[i] - count - correctSum[i];
         }
         return count;
-
+    }
+    public int MinOperationsII(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            return 0;
+        int count = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] <= nums[i - 1])
+            {
+                count += nums[i - 1] - nums[i] + 1;
+                nums[i] = Math.Max(nums[i - 1] + 1, nums[i]);
+            }
+        }
+        return count;
     }
     static void Main(string[] args)
     {
