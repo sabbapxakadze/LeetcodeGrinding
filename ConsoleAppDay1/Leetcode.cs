a46ff5c844574590a283650bcc8d0b6e0e347990
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-12-08 17:27:48 +0400

    SeatReservationManager

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -9648,6 +9648,26 @@ public class Leetcode
         Backtrack();
         return l.Count >= k ? l[k - 1] : "";
     }
+    public class SeatManager
+    {
+        PriorityQueue<int, int> seats;
+        public SeatManager(int n)
+        {
+            seats = new PriorityQueue<int, int>();
+            for (int i = 1; i < n + 1; i++)
+            {
+                seats.Enqueue(i, i);
+            }
+        }
+        public int Reserve()
+        {
+            return seats.Dequeue();
+        }
+        public void Unreserve(int seatNumber)
+        {
+            seats.Enqueue(seatNumber, seatNumber);
+        }
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
