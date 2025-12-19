e38ad17914e3de44c35aa54dc0318414e5f97cd7
Saba Pkhakadze <xdxsabuka@gmail.com>
2025-12-19 23:37:01 +0400

    NumSubsequence

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -10179,6 +10179,34 @@ public class Leetcode
         }
         return res;
     }
+    public int NumSubseq(int[] nums, int target)
+    {
+        const int MOD = 1_000_000_007;
+        Array.Sort(nums);
+
+        int n = nums.Length;
+        long[] pow2 = new long[n];
+        pow2[0] = 1;
+        for (int i = 1; i < n; i++)
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+
+        int left = 0, right = n - 1;
+        long result = 0;
+
+        while (left <= right)
+        {
+            if (nums[left] + nums[right] <= target)
+            {
+                result = (result + pow2[right - left]) % MOD;
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return (int)result;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
