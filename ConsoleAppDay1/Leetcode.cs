f84ef4fd0f15546b5eb703c4d3be98592f3caa48
Saba Pkhakadze <xdxsabuka@gmail.com>
2026-01-21 19:05:43 +0400

    AddTwoNumbers

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -12245,6 +12245,30 @@ public class Leetcode
         }
         return pq.Dequeue();
     }
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int carry = 0;
+        var dummy = new ListNode(0);
+        var curr = dummy;
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
