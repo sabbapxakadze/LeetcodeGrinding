4107691045ea5ac5908bcb042d292d3a2df15084
Saba Pkhakadze <xdxsabuka@gmail.com>
2026-01-27 14:51:00 +0400

    FindCommonCharacters

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -12735,6 +12735,27 @@ public class Leetcode
         }
         return true;
     }
+    public IList<string> CommonChars(string[] words)
+    {
+        if (words == null || words.Length == 0)
+            return new List<string>();
+        int[] minFreq = new int[26];
+        Array.Fill(minFreq, int.MaxValue);
+
+        foreach (var word in words)
+        {
+            int[] freq = new int[26];
+            foreach (var c in word)
+                freq[c - 'a']++;
+            for (int i = 0; i < 26; i++)
+                minFreq[i] = Math.Min(minFreq[i], freq[i]);
+        }
+        var res = new List<string>();
+        for (int i = 0; i < 26; i++)
+            for (int j = 0; j < minFreq[i]; j++)
+                res.Add(((char)(i + 'a')).ToString());
+        return res;
+    }
     static void Main(string[] args)
     {      
         Console.WriteLine();
