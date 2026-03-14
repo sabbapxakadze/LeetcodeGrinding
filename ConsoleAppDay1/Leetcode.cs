44ee840673f304025d532bfa223a62474f2953dd
Saba Pkhakadze <xdxsabuka@gmail.com>
2026-03-14 18:10:45 +0400

    CountAndSay

--- ConsoleAppDay1/Leetcode.cs
+++ ConsoleAppDay1/Leetcode.cs
@@ -14199,6 +14199,34 @@ public class Leetcode
         }
         return result;
     }
+    public string CountAndSay(int n)
+    {
+        if (n == 1)
+            return "1";
+        string result = "1";
+        for (int i = 2; i <= n; i++)
+        {
+            var sb = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < result.Length; j++)
+            {
+                if (result[j] == result[j - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Append(count);
+                    sb.Append(result[j - 1]);
+                    count = 1;
+                }
+            }
+            sb.Append(count);
+            sb.Append(result[result.Length - 1]);
+            result = sb.ToString();
+        }
+        return result;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine();
